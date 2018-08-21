9ad563cd001b165d90636482977600213012c6c6
abadawi2 <abadawi@terpmail.umd.edu>
2018-08-21 19:15:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  array.unshift (element);
+  return array;
+}
+
+
+function addElementToBeginningOfArray (array, element) {
+
+(...array,element);
+return array;
+}
