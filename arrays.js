1805bbe7610b3b44e69120fff3a2ad96577b16f4
abadawi2 <abadawi@terpmail.umd.edu>
2018-08-21 19:17:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element){
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  (elemet,...array);
+  return array;
+}
